4e2f11bac49fed2993a41998bda20782c985df25
Dr. Frewen <sfrewen@flintridgeprep.org>
2018-10-09 09:28:25 -0700

    blah

--- Homework1/Swole.java
+++ Homework1/Swole.java
@@ -1,9 +1,29 @@
 package Homework1;
 
-
 public class Swole
 {
-   public static void yell(){
-       System.out.println("LET'S DO THIS BRAAAAH");
-   }
+    public static long gcd(long a, long b){
+        int d = 0;
+        while ((a%2 == 0) && (b%2==0)) {
+            a = a/2;
+            b = b/2;
+            d++;
+        }
+        while (a != b) {
+            if (a%2 == 0) {
+                a = a/2;
+            } else if (b%2 == 0) {
+                b = b/2;
+            } else if (a > b) {
+                a = (a-b)/2;
+            } else {
+                b = (b-a)/2;
+            }
+        }
+        return a * (long) Math.pow(2, d);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(100, 20));
+    }
 }
---
+++ Homework1/VariableTest.java
@@ -0,0 +1,30 @@
+package Homework1;
+
+
+/**
+ * Write a description of class asdfs here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class VariableTest
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class asdfs
+     */
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
